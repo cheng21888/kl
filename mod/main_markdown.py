c3397bbe6d622319366781d9474e072a2637add1
cheng21888 <139730687+cheng21888@users.noreply.github.com>
2026-02-08 15:09:00 +0800

    Add files via upload

--- mod/main_markdown.py
+++ mod/main_markdown.py
@@ -180,6 +180,7 @@ def render_auction_report_tab(selected_date=None, prev_date=None):
         with tab_auto:
             st.subheader("🤖 题材共振监控")
             auto_df = data["auto_df"].copy()
+            auto = data["auto"].copy()
             auto_df['is_62'] = (
                     (auto_df['家数'] > 10) & (auto_df['红盘率%'] > 75) &
                     (auto_df['平均涨跌%'] > 1.2) & (auto_df['资金增量(亿)'] > 1)
