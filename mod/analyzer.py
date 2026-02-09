d55d9bb4c09ef6dc9521345e1996ea3ccf80349b
cheng21888 <139730687+cheng21888@users.noreply.github.com>
2026-02-09 17:15:00 +0800

    Update analyzer.py

--- mod/analyzer.py
+++ mod/analyzer.py
@@ -302,13 +302,11 @@ def calculate_auto(df: pd.DataFrame) -> pd.DataFrame:
 
     final = concept_grp.merge(leaders[['题材名称', '增量先锋']], on='题材名称', how='left')
     final = final[
-    (
-        (final['家数'] >= 4) & (final['家数'] <= 100) & 
+        ((final['家数'] >= 4) & (final['家数'] <= 100) & 
         (final['资金增量_亿'] > 0.3) & (final['平均涨跌_val'] > 0)
-    ) | (
-        (final['家数'] >= 4) & (final['家数'] <= 1000) & 
+        ) | ((final['家数'] >= 4) & (final['家数'] <= 1000) & 
         (final['资金增量_亿'] > 0.5) & (final['平均涨跌_val'] < -3)
-    )
+        )
     ]
 
     
