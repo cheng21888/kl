a988e5bf907a64660b8ce117d8079127049a05ec
cheng21888 <139730687+cheng21888@users.noreply.github.com>
2026-03-01 13:17:29 +0800

    Update bankuai.py

--- mod/bankuai.py
+++ mod/bankuai.py
@@ -358,6 +358,7 @@ def report_continuous_limit_up_acceleration(today_date: datetime, prev_date: dat
     df_analysis['涨跌幅'] = pd.to_numeric(df_analysis['涨跌幅'], errors='coerce').fillna(0)
     df_analysis['竞价金额_今'] = pd.to_numeric(df_analysis['竞价金额_今'], errors='coerce').fillna(0)
     倍数=df_analysis['竞价金额_今'] / df_analysis['昨日竞价成交额']
+    大=df_analysis['今日竞价涨跌幅']> (df_analysis['昨日竞价涨跌幅']
     # 应用筛选条件
     condition = (
         ((df_analysis['连续涨停天数'] >= 2) &
@@ -375,8 +376,10 @@ def report_continuous_limit_up_acceleration(today_date: datetime, prev_date: dat
          (倍数>0.9) & (df_analysis['涨跌幅']<9) & (df_analysis['昨日竞价涨跌幅']<1) & (倍数<1.2)) |
         ((df_analysis['涨跌幅'] <-5) & (df_analysis['昨日竞价涨跌幅']>-0.5) & (df_analysis['昨日收盘涨跌幅']>8) &
          (倍数>2)) |
-        ((df_analysis['涨跌幅'] >0.5) & (df_analysis['昨日竞价涨跌幅']>2) & (df_analysis['昨日振幅']>7) & (df_analysis['昨日收盘涨跌幅']<7)&
-         (倍数>1) & (df_analysis['昨日振幅']<10))
+        ((df_analysis['涨跌幅'] >0.5) & (df_analysis['昨日竞价涨跌幅']>2) & (df_analysis['昨日振幅']>7) & (df_analysis['昨日收盘涨跌幅']<7)) & (df_analysis['昨日收盘涨跌幅']>0) &
+         (倍数>1) & (df_analysis['昨日振幅']<10) |
+        ((df_analysis['涨跌幅'] >4) & (df_analysis['昨日竞价涨跌幅']>2) & (df_analysis['昨日收盘涨跌幅']>9.8 & 大 & 倍数>2)|
+        ((df_analysis['涨跌幅'] >6) & (df_analysis['昨日竞价涨跌幅']>2) & (df_analysis['昨日收盘涨跌幅']>9.8 & 大 & 倍数>5)) 
     )
     
     df_filtered = df_analysis[condition].copy()
